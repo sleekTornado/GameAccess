b0e330dd376589259d8cbda1a9909aace6bb8d21
Rayhan Makarim <rayhan.mb@yahoo.com>
2018-04-04 11:48:34 +0700

    Delete unnecesary part from example

--- app/src/main/java/com/sleektornado/gameaccess/MainActivity.java
+++ app/src/main/java/com/sleektornado/gameaccess/MainActivity.java
@@ -43,25 +43,8 @@ public class MainActivity extends AppCompatActivity
         }
     }
 
-//    @Override
-//    public boolean onCreateOptionsMenu(Menu menu) {
-//        // Inflate the menu; this adds items to the action bar if it is present.
-//        getMenuInflater().inflate(R.menu.main, menu);
-//        return true;
-//    }
-
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
         return super.onOptionsItemSelected(item);
     }
 
@@ -71,17 +54,13 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if(id == R.id.nav_hot){
 
-        } else if (id == R.id.nav_gallery) {
+        }else if(id == R.id.nav_special){
 
-        } else if (id == R.id.nav_slideshow) {
+        }else if(id == R.id.nav_preference){
 
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
+        }else if(id == R.id.nav_findGame){
 
         }
 
